b4e87a63ffd83b9b7e4d60b323c790b92df16b4a
rgutwein <51237807+rgutwein@users.noreply.github.com>
2022-11-17 00:17:54 -0500

    Update uuidtest.c

--- uuids/uuidtest.c
+++ uuids/uuidtest.c
@@ -2,6 +2,25 @@
 #include <string.h>
 #include <uuid/uuid.h>
 
+#define UUIDTEXTSIZE (sizeof(uuid_t)*2) + 5
+
+void printrawuuid(uuid_t id) {
+    printf("RAW: ");
+    for (int i=0 i < sizeof(uuid_t); i++) {
+        printf("%x ", id[i]);
+    }
+    printf("\n");
+}
+
+
 int main() {
+    uuid_t oscaluuid;
+    uuid_generate(oscaluuid);
+    printrawuuid(oscaluuid);
+    
+    char uuidtext[UUIDTEXTSIZE];
+    uuid_unparse(oscaluuid, uuidtext);
+    printf("UUID: %s\n", uuidtext);
+  
   
 }
